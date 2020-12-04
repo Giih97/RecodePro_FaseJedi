06d2d18bc4c9dbf35fca6c06736b7d8d9a5ffd76
Gilda Monteiro <67397737+Giih97@users.noreply.github.com>
2020-12-04 17:39:28 -0300

    Update fstackeletro.sql

--- back-end/bd/fstackeletro.sql
+++ back-end/bd/fstackeletro.sql
@@ -10,7 +10,19 @@ CREATE TABLE `produtos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-REPLACE INTO `produtos` (`id`, `categoria`, `descricao`, `preco`, `preco_venda`, `imagem`) VALUES (1,'geladeira','Eletrolux 2 Portas Cycle Defrost 260L 217v Branco',1700.89,1444.9,'GeladeiraElectroluxPortasCycleDefrostLvBranco.jpg'),(2,'geladeira',' Continental Frost Free - Duplex 370L TC41S',2000,1800.49,'ContinentalFrostFreeDuplex370LTC41S.jpg'),(3,'geladeira',' Continental Frost Free - Duplex 472L TC56',2500,2249.49,'ContinentalFrostFreeDuplexBranco472LTC56.jpg'),(4,'fogao','  Consul Acendimento Automático - CFO4 NARUNA',800.99,750.89,'Fogao4BocasConsulAcendimentoAutomaticoCFO4NARUNA.jpg'),(5,'fogao','5 Bocas Consul Acendimento Automático',800.99,750.89,'Fogao5BocasConsulAcendimentoAutomatico.jpg'),(6,'microondas','Electrolux com Painel Integrado 31L MI41S - 220 Volts',750.11,611.1,'Micro-ondasElectroluxcomPainelIntegrado31LMI41S220Volts.jpg'),(7,'microondas','Micro-ondas Electrolux MTD30-20L.jpg',2000,1800.49,'Micro-ondasElectroluxMTD30-20L.jpg'),(8,'microondas','Microondas LGE asy Clean 30 LBranco 220VMS3052RA.jpg',2000,1800.49,'MicroondasLGEasyClean30LBranco220VMS3052RA.jpg'),(9,'Lavadora de Roupas','  Consul CWB09 ABANA - 9Kg 15 Programas de Lavagem',1900.89,1089.9,'LavadoradeRoupasConsulCWB09ABANA-9Kg15ProgramasdeLavagem.jpg'),(10,'Lavadora de Roupas','  Electrolux LAC13 - 13Kg 12 Programas de Lavagem',1700.89,1400.9,'LavadoradeRoupasElectroluxLAC13-13Kg12ProgramasdeLavagem.jpg'),(11,'Lava_louca',' Brastemp Ative! BLF08 AB - 8 Serviços',2077.89,1888.99,'LavaLoucasBrastempAtiveBLF08AB8Servicos.jpg'),(12,'lava_louca',' Branca 8 Serviços (LV08B) - Electrolux',2500.89,2089.19,'LavaloucaBranca8ServiçosLV08BElectrolux.jpg');
+REPLACE INTO `produtos` (`id`, `categoria`, `descricao`, `preco`, `preco_venda`, `imagem`)
+VALUES (1,'geladeira','Eletrolux 2 Portas Cycle Defrost 260L 217v Branco',1700.89,1444.9,'GeladeiraElectroluxPortasCycleDefrostLvBranco.jpg'),
+(2,'geladeira',' Continental Frost Free - Duplex 370L TC41S',2000,1800.49,'ContinentalFrostFreeDuplex370LTC41S.jpg'),
+(3,'geladeira',' Continental Frost Free - Duplex 472L TC56',2500,2249.49,'ContinentalFrostFreeDuplexBranco472LTC56.jpg'),
+(4,'fogao','  Consul Acendimento Automático - CFO4 NARUNA',800.99,750.89,'Fogao4BocasConsulAcendimentoAutomaticoCFO4NARUNA.jpg'),
+(5,'fogao','5 Bocas Consul Acendimento Automático',800.99,750.89,'Fogao5BocasConsulAcendimentoAutomatico.jpg'),
+(6,'microondas','Electrolux com Painel Integrado 31L MI41S - 220 Volts',750.11,611.1,'Micro-ondasElectroluxcomPainelIntegrado31LMI41S220Volts.jpg'),
+(7,'microondas','Micro-ondas Electrolux MTD30-20L.jpg',2000,1800.49,'Micro-ondasElectroluxMTD30-20L.jpg'),
+(8,'microondas','Microondas LGE asy Clean 30 LBranco 220VMS3052RA.jpg',2000,1800.49,'MicroondasLGEasyClean30LBranco220VMS3052RA.jpg'),
+(9,'Lavadora de Roupas','  Consul CWB09 ABANA - 9Kg 15 Programas de Lavagem',1900.89,1089.9,'LavadoradeRoupasConsulCWB09ABANA-9Kg15ProgramasdeLavagem.jpg'),
+(10,'Lavadora de Roupas','  Electrolux LAC13 - 13Kg 12 Programas de Lavagem',1700.89,1400.9,'LavadoradeRoupasElectroluxLAC13-13Kg12ProgramasdeLavagem.jpg'),
+(11,'Lava_louca',' Brastemp Ative! BLF08 AB - 8 Serviços',2077.89,1888.99,'LavaLoucasBrastempAtiveBLF08AB8Servicos.jpg'),
+(12,'lava_louca',' Branca 8 Serviços (LV08B) - Electrolux',2500.89,2089.19,'LavaloucaBranca8ServiçosLV08BElectrolux.jpg');
 
 
 DROP TABLE IF EXISTS `pedidos`;
@@ -47,5 +59,15 @@ voltagem varchar (10),
 primary key (id_voltagem),
 foreign key (id_categoria) references produtos (id)
 );
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '5' WHERE (`id_voltagem` = '5');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '6', `voltagem` = '110' WHERE (`id_voltagem` = '6');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '7' WHERE (`id_voltagem` = '7');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '8' WHERE (`id_voltagem` = '8');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '9' WHERE (`id_voltagem` = '9');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '10' WHERE (`id_voltagem` = '10');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '11' WHERE (`id_voltagem` = '11');
+UPDATE `fstackeletro`.`voltagem` SET `id_categoria` = '12', `voltagem` = '220' WHERE (`id_voltagem` = '12');
+
+
 select * from voltagem join produtos on produtos.id = voltagem.id_voltagem;
 
